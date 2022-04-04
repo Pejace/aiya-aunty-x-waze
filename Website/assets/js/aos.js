540c99a08df669b6cf9060efa262e87529974aca
Pejace <102935640+Pejace@users.noreply.github.com>
2022-04-04 10:51:40 +0800

    Add files via upload

---
+++ Website/assets/js/aos.js
@@ -0,0 +1,9 @@
+AOS.init({
+
+    offset: 100, // offset (in px) from the original trigger point
+
+    delay: 0, // values from 0 to 3000, with step 50ms
+
+    duration: 800 // values from 0 to 3000, with step 50ms
+
+  });
